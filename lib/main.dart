55b8a071352b3741c4dbc418842e153ae111511b
Eleanor <atimeleanor@gmail.com>
2026-07-14 12:19:25 +0300

    added missing import and fixed errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-// Imports your login screen from your views directory
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'views/auth/login_screen.dart'; 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const UrPlugApp());
 }
 
